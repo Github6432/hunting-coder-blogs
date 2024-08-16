5ed59d2ccd92bd39d4003c8a6acfa4d1849627a3
Github6432 <parasshankhwar2@gmail.com>
2024-08-16 18:17:22 +0530

    show blog in blog.js from api

--- huntingcoder/pages/blog.js
+++ huntingcoder/pages/blog.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/Blog.module.css'
 import Link from 'next/link';
 
+// Step 1: Collect all the files from the blogdata directory
+// Step 2: Iterate through them and Display them
+
+
 const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    console.log("useeffect is running");
+    fetch('http://localhost:3000/api/blogs').then((a) => {
+      return a.json();
+    })
+      .then((parsed) => {
+        console.log(parsed)
+        setBlogs(parsed)
+      })
+  }, [])
+
   return <div className={styles.container}>
     <main className={styles.main}>
-      <div className={styles.blogItem}>
-        <Link href={'/blogpost/learn-javascript'}>
-          <h3 className={styles.blogItemh3}>How to learn JavaScript in 2022?</h3>
-        </Link>
-        <p>JavaScript is the language used to design logic for the web</p>
-      </div>
-      <div className={styles.blogItem}>
-        <h3>How to learn JavaScript in 2022?</h3>
-        <p>JavaScript is the language used to design logic for the web</p>
-      </div>
-      <div className={styles.blogItem}>
-        <h3>How to learn JavaScript in 2022?</h3>
-        <p>JavaScript is the language used to design logic for the web</p>
-      </div>
+      {blogs.map((blogitem) => {
+        return <div key={blogitem.slug}>
+          <Link href={`/blogpost/${blogitem.slug}`}>
+            <h3 className={styles.blogItemh3}>{blogitem.title}</h3></Link>
+          <p className={styles.blogItemp}>{blogitem.content.substr(0, 140)}...</p>
+          {/* <hr /> */}
+        </div>
+      })}
     </main>
   </div>
-};
+}
 
 export default Blog;
